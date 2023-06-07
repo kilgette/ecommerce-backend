4f2c80be994a11a4d1ebcd16e428e46d11e74f09
kilgette <kilgette@gmail.com>
2023-06-07 16:29:30 -0500

    fixes models

--- models/index.js
+++ models/index.js
@@ -15,8 +15,20 @@ Category.hasMany(Product, {
   onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
